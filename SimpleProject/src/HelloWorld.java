b46e9e029659bdce8f7dea28aae59388a666112d
joon_park <joon_park@NY01-8PS8CY1.mhf.mhc>
2016-10-19 11:46:52 -0400

    Initial commit

---
+++ SimpleProject/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/* 
+ * The purpose of this project is simply to 
+ * go through a feature workflow to get ourselves
+ * acquainted with git and github. This is a simple 
+ * Hello world class that we will work on and add
+ * additional features to. -Joon
+*/
+
+public class HelloWorld {
+
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}
+}
