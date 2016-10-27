dd6efa4d01dbf54758848fa8740bd9a7bac96887
Tarak <tarak.tikam@spglobal.com>
2016-10-27 16:57:30 -0400

    Added another method

--- SimpleProject/src/HelloWorld.java
+++ SimpleProject/src/HelloWorld.java
@@ -11,5 +11,11 @@ public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Inside tarak's branch...");
 		
+		HelloWorld hw = new HelloWorld();
+		hw.display("Another message");
+	}
+	
+	private void display(String message){
+		System.out.println(message);
 	}
 }
