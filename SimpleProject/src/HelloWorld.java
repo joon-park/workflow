efb0c137f690291fdb0a88c97295a3aac0153e54
ritesh_patel1 <ritesh.patel1@spdji.com>
2016-10-27 16:37:38 -0400

    Commited Ritesh's Hello world!

--- SimpleProject/src/HelloWorld.java
+++ SimpleProject/src/HelloWorld.java
@@ -9,7 +9,7 @@
 public class HelloWorld {
 
 	public static void main(String args[]){
-		System.out.println("Hello World -- Welcome to Ritesh branch");
+		System.out.println("Hello World -- Welcome to Ritesh branch!");
 		
 		HelloWorld hello = new HelloWorld();
 		hello.printMessage("Hello Git");
